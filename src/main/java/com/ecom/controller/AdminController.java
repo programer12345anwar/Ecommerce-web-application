1350d9e76619417cc5c6de3986eedd0e77e0214f
programer12345anwar <mdanwar40212@gmail.com>
2024-10-22 15:34:51 +0530

    "updation in Category"

--- src/main/java/com/ecom/controller/AdminController.java
+++ src/main/java/com/ecom/controller/AdminController.java
@@ -1,14 +1,25 @@
 package com.ecom.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ecom.model.Category;
+import com.ecom.service.CategoryService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-	@GetMapping("")
+	@Autowired
+	private CategoryService categoryService;
 
+	@GetMapping("")
 	public String admin() {
 		return "admin/index";
 	}
@@ -23,5 +34,27 @@ public class AdminController {
 
 	public String category() {
 		return "admin/category";
+	}
+
+	@PostMapping("/saveCategory")
+	public String saveCategory(@ModelAttribute Category category, HttpSession session) {
+		Boolean existCategory = categoryService.existCategory(category.getName());
+		if (existCategory) {
+
+			session.setAttribute("errorMsg", "Category Name Already exist ");
+		} else {
+			Category saveCategory = categoryService.saveCategory(category);
+			if (ObjectUtils.isEmpty(saveCategory)) {
+				session.setAttribute("errorMsg", "Not saved ! internal server error");
+
+			} else {
+				session.setAttribute("succMsg", "saved successfully");
+
+			}
+
+		}
+
+		categoryService.saveCategory(category);
+		return "redirect:/category";
 	}
 }
